cd76bb1cf9715cdfca17338ff2e94a7a4cc6b38e
legacychimera247 <mad_fer_it@vivaldi.net>
2024-09-03 13:24:11 +0200

    Add files via upload

---
+++ translations/Septitrac-it.ts
@@ -0,0 +1,267 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="it_IT">
+<context>
+    <name>About</name>
+    <message>
+        <source>About</source>
+        <translation>Info</translation>
+    </message>
+    <message>
+        <source>Made by Michał Szczepaniak.</source>
+        <translation>Sviluppato da Michał Szczepaniak.</translation>
+    </message>
+    <message>
+        <source>Licensed under GPLv3</source>
+        <translation>Sotto licenza GPLv3</translation>
+    </message>
+    <message>
+        <source>Links</source>
+        <translation>Collegamenti</translation>
+    </message>
+    <message>
+        <source>Thanks to</source>
+        <translation>Grazie a</translation>
+    </message>
+    <message>
+        <source>Fernando Lanero for being awesome testers.</source>
+        <translation>Fernando Lanero per essere un tester fantastico.</translation>
+    </message>
+    <message>
+        <source>Greg Goncharov (@gregguh at telegram) for icons.</source>
+        <translation>Greg Goncharov (@gregguh su telegram) per le icone.</translation>
+    </message>
+    <message>
+        <source>Fernando Lanero for spanish translation.</source>
+        <translation>Fernando Lanero per la traduzione spagnola.</translation>
+    </message>
+    <message>
+        <source>Donate</source>
+        <translation>Dona</translation>
+    </message>
+</context>
+<context>
+    <name>CoverPage</name>
+    <message>
+        <source>Septitrac</source>
+        <translation>Septitrac</translation>
+    </message>
+    <message>
+        <source>Name:</source>
+        <translation>Nome:</translation>
+    </message>
+    <message>
+        <source>Status:</source>
+        <translation>Stato:</translation>
+    </message>
+    <message>
+        <source>Last update:</source>
+        <translation>Ultimo aggiornamento:</translation>
+    </message>
+</context>
+<context>
+    <name>Main</name>
+    <message>
+        <source>Devices</source>
+        <translation>Dispositivi</translation>
+    </message>
+    <message>
+        <source>Device</source>
+        <translation>Dispositivo</translation>
+    </message>
+    <message>
+        <source>Name:</source>
+        <translation>Nome:</translation>
+    </message>
+    <message>
+        <source>Status:</source>
+        <translation>Stato:</translation>
+    </message>
+    <message>
+        <source>Last update:</source>
+        <translation>Ultimo aggiornamento:</translation>
+    </message>
+    <message>
+        <source>Phone:</source>
+        <translation>Telefono:</translation>
+    </message>
+    <message>
+        <source>Model:</source>
+        <translation>Modello:</translation>
+    </message>
+    <message>
+        <source>Category:</source>
+        <translation>Categoria:</translation>
+    </message>
+    <message>
+        <source>Settings</source>
+        <translation>Impostazioni</translation>
+    </message>
+    <message>
+        <source>Reports</source>
+        <translation>Segnalazioni</translation>
+    </message>
+    <message>
+        <source>About</source>
+        <translation>Info</translation>
+    </message>
+</context>
+<context>
+    <name>Reports</name>
+    <message>
+        <source>Configure</source>
+        <translation>Configura</translation>
+    </message>
+    <message>
+        <source>Save route as gpx</source>
+        <translation>Salva percorso come gpx</translation>
+    </message>
+    <message>
+        <source>Reports</source>
+        <translation>Segnalazioni</translation>
+    </message>
+    <message>
+        <source>Properties</source>
+        <translation>Proprietà</translation>
+    </message>
+    <message>
+        <source>Device:</source>
+        <translation>Dispositivo:</translation>
+    </message>
+    <message>
+        <source>Max speed:</source>
+        <translation>Velocità massima:</translation>
+    </message>
+    <message>
+        <source>Average Speed:</source>
+        <translation>Velocità media:</translation>
+    </message>
+    <message>
+        <source>Distance:</source>
+        <translation>Distanza:</translation>
+    </message>
+    <message>
+        <source>Spent fuel:</source>
+        <translation>Carburante speso:</translation>
+    </message>
+    <message>
+        <source>Duration:</source>
+        <translation>Durata:</translation>
+    </message>
+    <message>
+        <source>Start:</source>
+        <translation>Partenza:</translation>
+    </message>
+    <message>
+        <source>End:</source>
+        <translation>Fine:</translation>
+    </message>
+    <message>
+        <source>Driver:</source>
+        <translation>Guidatore:</translation>
+    </message>
+    <message>
+        <source>Engine hours:</source>
+        <translation>Ore motore:</translation>
+    </message>
+    <message>
+        <source>%1 km/h</source>
+        <translation>%1 km/h</translation>
+    </message>
+    <message>
+        <source>%1 km</source>
+        <translation>%1 km</translation>
+    </message>
+    <message>
+        <source>%1l</source>
+        <translation>%1l</translation>
+    </message>
+    <message>
+        <source>Type</source>
+        <translation>Tipo</translation>
+    </message>
+    <message>
+        <source>Route</source>
+        <translation>Percorso</translation>
+    </message>
+    <message>
+        <source>Trips</source>
+        <translation>Viaggi</translation>
+    </message>
+    <message>
+        <source>Stops</source>
+        <translation>Fermate</translation>
+    </message>
+    <message>
+        <source>Summary</source>
+        <translation>Sommario</translation>
+    </message>
+    <message>
+        <source>Device</source>
+        <translation>Dispositivo</translation>
+    </message>
+    <message>
+        <source>Group</source>
+        <translation>Gruppo</translation>
+    </message>
+    <message>
+        <source>Period</source>
+        <translation>Periodo</translation>
+    </message>
+    <message>
+        <source>Today</source>
+        <translation>Oggi</translation>
+    </message>
+    <message>
+        <source>Yesterday</source>
+        <translation>Ieri</translation>
+    </message>
+    <message>
+        <source>This Week</source>
+        <translation>Questa settimana</translation>
+    </message>
+    <message>
+        <source>Previous Week</source>
+        <translation>Settimana precedente</translation>
+    </message>
+    <message>
+        <source>This Month</source>
+        <translation>Questo mese</translation>
+    </message>
+    <message>
+        <source>Previous Month</source>
+        <translation>Mese precedente</translation>
+    </message>
+    <message>
+        <source>Save route as image</source>
+        <translation>Salva percorso come immagine</translation>
+    </message>
+</context>
+<context>
+    <name>Settings</name>
+    <message>
+        <source>Settings</source>
+        <translation>Impostazioni</translation>
+    </message>
+    <message>
+        <source>Password</source>
+        <translation>Password</translation>
+    </message>
+    <message>
+        <source>Connect</source>
+        <translation>Connetti</translation>
+    </message>
+    <message>
+        <source>Connected!</source>
+        <translation>Connesso!</translation>
+    </message>
+    <message>
+        <source>Server</source>
+        <translation>Server</translation>
+    </message>
+    <message>
+        <source>Login</source>
+        <translation>Entra</translation>
+    </message>
+</context>
+</TS>
